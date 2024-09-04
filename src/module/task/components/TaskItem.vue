e664c4258fea4aefdd4ed418fe6e0c9251445d17
Hernan Yarbi <hernan.yarbi@gmail.com>
2024-09-03 21:36:50 -0300

    added edit task and updated

--- src/module/task/components/TaskItem.vue
+++ src/module/task/components/TaskItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center gap-2">
+  <div class="flex items-center gap-2 w-full">
     <input
       id="link-checkbox"
       type="checkbox"
@@ -7,27 +7,67 @@
       class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
     />
 
-    <p>{{ task.description }}</p>
+    <p v-if="!editMode" class="p-1">{{ task.description }}</p>
+    <input
+      v-else
+      @keyup.enter="updateChanges"
+      type="text"
+      name="task"
+      id="task"
+      class="border p-1 border-red-500 rounded-lg w-full max-w-sm"
+      v-model="description"
+    />
   </div>
   <div class="flex justify-end items-center">
     <div class="flex space-x-2">
-      <ToolTip text="Puedes considerar dividir tus tareas." position="bottom" v-if="task.isComplex">
-        <div class="cursor-pointer"><InfoCircleIcon /></div>
+      <ToolTip
+        text="Puedes considerar dividir esta tarea."
+        position="bottom"
+        v-if="task.isComplex && !task.subTask?.length && !editMode"
+      >
+        <div class="cursor-pointer" @click="enableEditMode"><CutIcon /></div>
+      </ToolTip>
+      <ToolTip
+        text="Puedes considerar dividir esta tarea."
+        position="bottom"
+        v-if="!task.isComplex && !editMode"
+      >
+        <div class="cursor-pointer" @click="enableEditMode"><EditIcon /></div>
+      </ToolTip>
+      <ToolTip text="Guardar Cambios" position="bottom" v-if="editMode">
+        <div class="cursor-pointer" @click="updateChanges"><TickSquareIcon /></div>
       </ToolTip>
     </div>
   </div>
-  <!-- Aquí se podrían agregar subtareas si la tarea es compleja -->
 </template>
 
 <script setup lang="ts">
 import ToolTip from '@/module/common/components/ToolTip.vue'
-import InfoCircleIcon from '@/module/common/icons/InfoCircleIcon.vue'
 import type { Task } from '../interfaces/task.interface'
 import { useTaskStore } from '../store/useTaskStore'
+import CutIcon from '@/module/common/icons/CutIcon.vue'
+import { ref } from 'vue'
+import TickSquareIcon from '@/module/common/icons/TickSquareIcon.vue'
+import EditIcon from '@/module/common/icons/EditIcon.vue'
 interface Props {
   task: Task
 }
-defineProps<Props>()
+const props = defineProps<Props>()
 
 const taskStore = useTaskStore()
+const editMode = ref(false)
+const description = ref(props.task.description)
+
+const enableEditMode = () => {
+  editMode.value = !editMode.value
+}
+
+const updateChanges = () => {
+  const updateTask = {
+    ...props.task,
+    description: description.value
+  }
+  taskStore.updateTask(updateTask)
+  editMode.value = false
+}
 </script>
